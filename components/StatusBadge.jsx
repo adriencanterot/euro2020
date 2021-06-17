8c248b988561bbe23f30fe84fb1cf2854efbc758
Adrien Cantérot <adriencanterot@macbook-pro-de-adrien.local>
2021-06-17 12:41:31 +0000

    added hours on « today » dates

---
+++ components/StatusBadge.jsx
@@ -0,0 +1,61 @@
+import { Center, Badge } from "@chakra-ui/react";
+
+function showDate(dateString) {
+	const date = new Date(dateString);
+	const options = {
+		weekday: "short",
+		month: "short",
+		day: "numeric",
+		hour: "numeric",
+	};
+	return date.toLocaleDateString("fr-FR", options);
+}
+
+function showTime(dateString) {
+	const date = new Date(dateString);
+	return date.getHours() + "H";
+}
+
+function isToday(someDate) {
+	const today = new Date();
+	return (
+		someDate.getDate() == today.getDate() &&
+		someDate.getMonth() == today.getMonth() &&
+		someDate.getFullYear() == today.getFullYear()
+	);
+}
+
+function StatusBadge(props) {
+	const { game } = props;
+	return (
+		<>
+			{game.left_score != undefined && (
+				<Center>
+					<Badge
+						fontSize="lg"
+						colorScheme="green"
+						alignContent="center"
+					>
+						{game.left_score} - {game.right_score}
+					</Badge>
+				</Center>
+			)}
+			{game.left_score == undefined && (
+				<Center>
+					<Badge
+						fontSize="xs"
+						colorScheme="orange"
+						alignContent="center"
+					>
+						{isToday(new Date(game.datetime)) &&
+							"Aujourd'hui à " + showTime(game.datetime)}
+						{!isToday(new Date(game.datetime)) &&
+							showDate(new Date(game.datetime))}
+					</Badge>
+				</Center>
+			)}
+		</>
+	);
+}
+
+export default StatusBadge;
