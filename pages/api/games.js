2ccbdcfa745ae252ea51e7e7202a1db5929d3408
Adrien Cantérot <adriencanterot@macbook-pro-de-adrien.local>
2021-06-21 00:26:24 +0000

    Removed legacy apollo code

--- pages/api/games.js
+++ pages/api/games.js
@@ -1,13 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { gql, ApolloClient, InMemoryCache } from "@apollo/client";
 import axios from "axios";
 import path from "path";
 import { getInitialState } from "../../lib/graphql";
-const client = new ApolloClient({
-	cache: new InMemoryCache(),
-	uri: path.join(process.env.STRAPI_CLIENT, "/graphql"),
-});
 
 export default async (req, res) => {
 	const response = await getInitialState();
